969030699ce2df17b4af88065fe7cfcacc317317
Gaspard Masson <78444117+GaspardMasson@users.noreply.github.com>
2023-04-23 19:27:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
-import requests
 from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, Worldddddddd!"
 @app.route('/webhook', methods=['POST'])
-def webhook():
+def handle_strava_webhook():
     if request.method == 'POST':
-        # Récupérer le payload JSON envoyé par Strava
-        payload = request.json
-
-        # Traiter le payload
+        data = request.json
+        # Gérer les données du webhook Strava ici
         # ...
-
-        # Répondre à Strava avec un code d'état HTTP 200 OK
-        return 'Success', 200
+        return 'success', 200
     else:
-        # Si la demande n'est pas une demande POST, renvoyer une erreur 400 Bad Request
-        return 'Bad Request', 400
+        abort(400)
 
 if __name__ == '__main__':
     app.run()
